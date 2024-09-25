f52fadde91467784c5cd4d44b0f2a3acc8217198
sirasavadadivya145 <sirasavadadivya@gmail.com>
2024-09-25 12:06:19 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,14 @@
+public static Calculation
+{
+public static void main(String[] args)
+{
+int i=10;
+System.out.println("printing list of first 10 even numbers\n");
+do
+{
+System.out.println(i);
+i=i+2;
+}
+while(i<=10);
+}
+}
